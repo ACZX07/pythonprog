f294f088a35942b4c23d68ca53046c6f22fb60aa
Abhra <138387535+ACZX07@users.noreply.github.com>
2023-07-03 12:36:34 +0530

    Update reverse multiplication table.py

--- reverse multiplication table.py
+++ reverse multiplication table.py
@@ -1,3 +1,5 @@
+#THIS PROGRAM PRINTS A REVERSE MULTIPLICATION TABLE FOR ANY NUMBER UPTO THE LIMIT SET BY THE USER 
+
 n = int(input("Enter the number you need the table of: "))
 
 lim = int(input("Enter the ending multiple: "))
