3c09b598a0c0c03c8fc5d8185cec229dea066aca
Abhra <138387535+ACZX07@users.noreply.github.com>
2023-07-03 12:39:47 +0530

    Update vowel or consonant and upper or lower.py

--- vowel or consonant and upper or lower.py
+++ vowel or consonant and upper or lower.py
@@ -1,3 +1,5 @@
+#THIS PROGRAMS CHECKS IF A LETTER IS UPPERCASE/LOWERCASE AND VOWEL/CONSONANT 
+
 letter = input("Enter any letter:")
 for i in letter:
     if (i == "a" or i=="e" or i=="i" or i=="o" or i=="u" or i == "A" or i == "E" or i == "I" or i == "O" or i == "U"):
